7c26286f4ce25ef2526fd49b58a64311aca0d467
Felipe_1270 <150533282+Felipe-1270@users.noreply.github.com>
2024-01-17 14:00:54 -0300

    Rascunho.sh

--- rascunho.sh
+++ rascunho.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 #Script para verificar metadados em arquivos de um determinado site
-#verificar se digitou argumento
-#se não
-#printa modo correto de usar
-#$0 site.com.br
-else;
+if ["$1" == ""]
+then
+  echo "### MISSING ARGUMENTS ###"
+  echo "Defina um site para realizar a busca"
+  echo "$0 site.com.br"
+else
+do
+  echo "Procurando documentos em $1"
   lynx --dump “https://google.com/search?&q=site:$1+ext:pdf+ext:doc+ext:xlms” | grep ".pdf|.doc|.xlms" | cut -d "=" -f2 | egrep -v "site|termo.inutil | sed 's/...$//' > arquivo_URLs #vai criar a pasta onde o script rodar, dá p designar uma caminho
+  echo "Salvando links encontrados"
 
+  echo "Baixando documentos encontrados"
   for url in $(cat arquivo_URLs);
     do wget -q $url;
     done
-
+  echo "Verificando metadados..."
   exiftool *.pdf
