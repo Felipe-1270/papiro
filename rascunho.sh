e0bbe1e2d0d4f59a26d655332812bf0f5b4cad57
Felipe_1270 <150533282+Felipe-1270@users.noreply.github.com>
2024-01-18 13:56:20 -0300

    rascunho.sh

--- rascunho.sh
+++ rascunho.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #Script para verificar metadados em arquivos de um determinado site
-if ["$1" == ""]
+if [ "$1" == "" ]
 then
   echo "### MISSING ARGUMENTS ###"
   echo "Defina um site para realizar a busca"
   echo "$0 site.com.br"
 else
   echo "Procurando documentos em $1"
-  lynx --dump "https://google.com/search?&q=site:$1+ext:pdf+ext:doc+ext:xlms" | grep ".pdf|.doc|.xlms" | cut -d "=" -f2 | egrep -v "site|termo.inutil | sed 's/...$//' > arquivo_URLs #vai criar a pasta onde o script rodar, dá p designar uma caminho
+  lynx --dump "https://google.com/search?&q=site:$1++ext:pdf++ext:doc++ext:xlms" | grep ".pdf|.doc|.xlms" | cut -d "=" -f2 | egrep -v "site" | sed 's/...$//' > arquivo_URLs #vai criar a pasta onde o script rodar, dá p designar uma caminho
   echo "Salvando links encontrados"
 
   echo "Baixando documentos encontrados"
   for url in $(cat arquivo_URLs);
     do wget -q $url;
-    done
+  done
   echo "Verificando metadados..."
   exiftool * -ext pdf -ext doc -ext xlms
 
-#ChatGPT validou, falta testar 
+fi
+#lynx ta crashando na EBnet
