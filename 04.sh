f6305ee5c8987117ca2983c4763e7ca2b38774d6
Felipe_1270 <150533282+Felipe-1270@users.noreply.github.com>
2024-01-22 19:32:05 -0300

    04.sh
    Script para verificar metadados em arquivos baixados

---
+++ 04.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Script para verificar metadados em arquivos de um determinado site
+if [ "$1" == "" ]
+then
+  echo "### MISSING ARGUMENTS ###"
+  echo "Defina um site para realizar a busca"
+  echo "$0 site.com.br"
+else
+  echo "Procurando documentos em $1"
+  lynx --dump "https://google.com/search?&q=site:$1++ext:pdf++ext:doc++ext:xlms" | grep ".pdf|.doc|.xlms" | cut -d "=" -f2 | egrep -v "site" | sed 's/...$//' > arquivo_URLs #vai criar a pasta onde o script rodar
+  echo "Salvando links encontrados"
+
+  echo "Baixando documentos encontrados"
+  for url in $(cat arquivo_URLs);
+    do wget -q $url;
+  done
+  echo "Verificando metadados..."
+  exiftool * -ext pdf -ext doc -ext xlms #vai ler todos arq da pasta
+
+fi
