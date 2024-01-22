4c6590949b413bd8b761aea8dcf75bb800e2d193
Felipe_1270 <150533282+Felipe-1270@users.noreply.github.com>
2024-01-22 19:26:50 -0300

    02.sh
    Varre os IPs da rede, batendo nas portas selecionadas

--- 02.sh
+++ 02.sh
@@ -1,1 +1,24 @@
-#Script p fazer outra tal coisa
+#!/bin/bash
+#Varre os IPs da rede informada, batendo nas portas informadas abaixo
+
+if [ "$#" -ne 1 ]; then
+  echo "Sintaxe Incorreta"
+  echo "Uso: $0 <faixa_de_IP>"
+  exit 1
+fi
+
+faixa_de_ip=$1
+portas="15,22,38,169,235" #informar as portas a serem testadas
+
+hping3_scan() {
+  for porta in $(echo "$2" | tr ',' ' '); do
+    hping3 -S -p "$porta" -c 1 "$1"
+  done
+}
+
+for i in $(seq 1 10); do #informar quais hosts devem ser testados
+  ip="$faixa_de_ip.$i"
+  hping3_scan "$ip" "$portas"
+done
+
+#trabalhar para melhorar a saída, tá um zrlho
