c26a4b9fb5cb2f6326f0d11f326f071fffd1928a
Felipe_1270 <150533282+Felipe-1270@users.noreply.github.com>
2024-01-22 19:29:30 -0300

    03.sh
    Script_Parsing_HTML

--- 03.sh
+++ 03.sh
@@ -1,1 +1,18 @@
-#Script3
+#!/bin/bash
+# Script_Parsing_HTML
+
+if [ "$1" == "" ]; then
+  echo "####################################"
+  echo "->         PARSING HTML           <-"
+  echo "-> MANDALORIAN_WORM - FLP.GE1270  <-"
+  echo "-> ./Parsing.sh www.alvo.com.br   <-"
+else
+  wget -q -O - "$1" | grep "href" | cut -d "/" -f 3 | grep \. | cut -d '"' -f 1 | grep -v "<l" > lista
+  if [ -f "lista" ]; then
+    while read -r url; do
+      host "$url" | grep "has address"
+    done < lista
+  else
+    echo "O arquivo 'lista' não foi gerado corretamente."
+  fi
+fi
